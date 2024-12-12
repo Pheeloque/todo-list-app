118a5de15be9d950afd2199e635f679c970dc196
Pheeloque <andrey.adamuk@gmail.com>
2024-12-12 07:16:12 +0300

    minor improvements

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,8 @@ import React, { useState } from "react";
 
 function App() {
   const [task, setTask] = useState("");
-<<<<<<< HEAD
   const [tasksList, setTasksList] = useState([]);
   const [editingTask, setEditingTask] = useState(null);
-=======
-  const [taskList, setTaskList] = useState([]);
-  const [editingTask, setEditingTask] = useState({});
->>>>>>> 34af34d42ad5bce90e85da5e48601f4bae4bfd74
 
   const handleInputChange = (event) => {
     setTask(event.target.value);
@@ -16,26 +11,17 @@ function App() {
 
   const handleAddTask = () => {
     if (task.trim() !== "") {
-<<<<<<< HEAD
       const newTask = { id: Date.now(), text: task.trim() };
       setTasksList((prevTasks) => [...prevTasks, newTask]);
-=======
-      const newTask = { id: Date.now(), text: task };
-      setTaskList((prevTasks) => [...prevTasks, newTask]);
->>>>>>> 34af34d42ad5bce90e85da5e48601f4bae4bfd74
       // console.log(`Добавлена задача: ${task}`);
       setTask("");
     }
   };
 
   const handleDeleteTask = (targetId) => {
-<<<<<<< HEAD
     setTasksList((prevTasks) =>
       prevTasks.filter((task) => task.id !== targetId)
     );
-=======
-    setTaskList(taskList.filter((task) => task.id !== targetId));
->>>>>>> 34af34d42ad5bce90e85da5e48601f4bae4bfd74
   };
 
   const handleEditTask = (task) => {
@@ -43,33 +29,16 @@ function App() {
   };
 
   const handleSaveEdit = () => {
-<<<<<<< HEAD
     if (!editingTask?.text.trim()) return;
     setTasksList((prevTasks) =>
       prevTasks.map((task) => (task.id === editingTask.id ? editingTask : task))
     );
     setEditingTask(null);
-=======
-    if (editingTask) {
-      setTaskList((prevTasks) =>
-        prevTasks.map((task) =>
-          task.id === editingTask.id
-            ? { ...task, text: editingTask.text }
-            : task
-        )
-      );
-      setEditingTask({});
-    }
->>>>>>> 34af34d42ad5bce90e85da5e48601f4bae4bfd74
   };
 
   return (
     <div>
-<<<<<<< HEAD
       <h1>To-Do list App</h1>
-=======
-      <h1>Список дел</h1>
->>>>>>> 34af34d42ad5bce90e85da5e48601f4bae4bfd74
       <div>
         <input
           type="text"
@@ -80,15 +49,9 @@ function App() {
         <button onClick={handleAddTask}>Добавить</button>
       </div>
       <ul>
-<<<<<<< HEAD
         {tasksList.map((task) => (
           <li key={task.id}>
             {editingTask?.id === task.id ? (
-=======
-        {taskList.map((task) => (
-          <li key={task.id}>
-            {editingTask.id === task.id ? (
->>>>>>> 34af34d42ad5bce90e85da5e48601f4bae4bfd74
               <>
                 <input
                   type="text"
@@ -100,29 +63,21 @@ function App() {
                     })
                   }
                 />
-<<<<<<< HEAD
                 <button
                   onClick={handleSaveEdit}
                   disabled={!editingTask?.text.trim()}
                 >
                   Сохранить
                 </button>
-=======
-                <button onClick={handleSaveEdit}>Сохранить</button>
->>>>>>> 34af34d42ad5bce90e85da5e48601f4bae4bfd74
               </>
             ) : (
               task.text
             )}
-<<<<<<< HEAD
             {editingTask?.id !== task.id && (
               <button onClick={() => handleEditTask(task)}>
                 Редактировать
               </button>
             )}
-=======
-            <button onClick={() => handleEditTask(task)}>Редактировать</button>
->>>>>>> 34af34d42ad5bce90e85da5e48601f4bae4bfd74
             <button onClick={() => handleDeleteTask(task.id)}>Удалить</button>
           </li>
         ))}
